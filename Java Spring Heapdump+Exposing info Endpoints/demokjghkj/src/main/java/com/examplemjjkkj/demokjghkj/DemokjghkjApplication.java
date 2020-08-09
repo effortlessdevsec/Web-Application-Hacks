97e9ecf51f56639b00e6a2434ea46cef3911cfe4
Effortless <30777722+effortlessdevsec@users.noreply.github.com>
2020-08-09 11:31:13 +0530

    Add files via upload

---
+++ Java Spring Heapdump+Exposing info Endpoints/demokjghkj/src/main/java/com/examplemjjkkj/demokjghkj/DemokjghkjApplication.java
@@ -0,0 +1,13 @@
+package com.examplemjjkkj.demokjghkj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokjghkjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemokjghkjApplication.class, args);
+    }
+
+}
---
+++ Java Spring Heapdump+Exposing info Endpoints/demokjghkj/src/test/java/com/examplemjjkkj/demokjghkj/DemokjghkjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplemjjkkj.demokjghkj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemokjghkjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
